2e924fbc3e203fb21cd47a9b17c9bd18784531d6
Evgeniya <zbizk322@mail.ru>
2019-11-11 17:20:51 +0700

    Реализация карточки сотрудника

--- WStore/WStore/Controllers/HomeController.cs
+++ WStore/WStore/Controllers/HomeController.cs
@@ -16,14 +16,21 @@ namespace WStore.Controllers
 
         private static readonly List<EmployeeView> _Employees = new List<EmployeeView>
         {
-            new EmployeeView{Id=1, FirstName="Иван", Patronymic="Иванович",SurName="Иванов", Age=25 },
-            new EmployeeView{Id=2, FirstName="Петр", Patronymic="Петрович",SurName="Петров", Age=45 },
-            new EmployeeView{Id=3, FirstName="Андрей", Patronymic="Андреевич",SurName="Андреев", Age=29 }
+            new EmployeeView{Id=1, FirstName="Иван", Patronymic="Иванович",SurName="Иванов", Age=25, City="Томск" },
+            new EmployeeView{Id=2, FirstName="Петр", Patronymic="Петрович",SurName="Петров", Age=45, City="Москва" },
+            new EmployeeView{Id=3, FirstName="Андрей", Patronymic="Андреевич",SurName="Андреев", Age=29, City="Екатеринбург" }
         };
 
         public IActionResult GetEmployes()
         {
             return View(_Employees);
+        }
+
+        public IActionResult Details(int Id)
+        {
+            foreach (var i in _Employees) if (i.Id==Id) return View(i);
+            
+            return View(_Employees[0]);
         }
     }
 }
--- WStore/WStore/Models/EmployeeView.cs
+++ WStore/WStore/Models/EmployeeView.cs
@@ -12,5 +12,6 @@ namespace WStore.Models
         public string SurName { get; set; }
         public string Patronymic { get; set; }
         public int Age { get; set; }
+        public string City { get; set; }
     }
 }
