6c4184bdcb36b0372882f098f3cd0325dff47878
Evgeniya <zbizk322@mail.ru>
2019-11-21 11:11:31 +0700

    Добавлены сущности и тестовые данные

---
+++ WStore/WStore/Infrasructure/Implementation/InMemoryProductData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WStore.Infrasructure.Interface;
+
+namespace WStore.Infrasructure.Implementation
+{
+    public class InMemoryProductData:IProductData
+    {
+    }
+}
---
+++ WStore/WStore/Infrasructure/Interface/IProductData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WStore.Infrasructure.Interface
+{
+    public interface IProductData
+    {
+    }
+}
