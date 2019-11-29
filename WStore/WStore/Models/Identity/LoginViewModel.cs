e8e3b312fa0278e4b37e489a1d82f03bb500d0d8
Евгения <zbizk322@mail.ru>
2019-11-29 19:37:15 +0700

    Регистрация и вход в систему

---
+++ WStore/WStore/Models/Identity/LoginViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WStore.Models.Identity
+{
+    public class LoginViewModel
+    {
+    }
+}
