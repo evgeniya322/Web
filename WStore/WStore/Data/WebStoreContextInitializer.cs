31f12c13546cc961d2c959a9000b1312cfed2d1b
Евгения <zbizk322@mail.ru>
2019-12-03 12:06:31 +0700

    Инициализация БД администратором и ролями

--- WStore/WStore/Data/WebStoreContextInitializer.cs
+++ WStore/WStore/Data/WebStoreContextInitializer.cs
@@ -1,21 +1,33 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebStore.DAL.Context;
+using WebStore.Domain.Entities.Identity;
 
 namespace WStore.Data
 {
     public class WebStoreContextInitializer
     {
         private readonly WebStoreContext _db;
-        public WebStoreContextInitializer(WebStoreContext db) => _db = db;
+        private readonly UserManager<User> _UserManager;
+        private readonly RoleManager<Role> _RoleManager;
+
+        public WebStoreContextInitializer(WebStoreContext db, UserManager<User> UserManager, RoleManager<Role> RoleManager)
+        {
+            _db = db;
+            _UserManager = UserManager;
+            _RoleManager = RoleManager;
+        }
 
         public async Task InitializeAsync()
         {
             var db = _db.Database;
             await db.MigrateAsync();
+
+            await IdentityInitializeAsync();
 
             if (await _db.Products.AnyAsync()) return;
 
@@ -50,6 +62,30 @@ namespace WStore.Data
                 await db.ExecuteSqlCommandAsync("SET IDENTITY_INSERT [dbo].[Products] OFF");
 
                 transaction.Commit();
+            }
+        }
+
+        private async Task IdentityInitializeAsync()
+        {
+            if (!await _RoleManager.RoleExistsAsync(Role.Administrator))
+                await _RoleManager.CreateAsync(new Role { Name = Role.Administrator });
+
+            if (!await _RoleManager.RoleExistsAsync(Role.User))
+                await _RoleManager.CreateAsync(new Role { Name = Role.User });
+
+            if (await _UserManager.FindByNameAsync(User.Administrator) is null)
+            {
+                var admin = new User
+                {
+                    UserName = User.Administrator,
+                };
+
+                var creation_result = await _UserManager.CreateAsync(admin, User.AdminPasswordDefault);
+
+                if (creation_result.Succeeded)
+                    await _UserManager.AddToRoleAsync(admin, Role.Administrator);
+                else
+                    throw new InvalidOperationException($"Ошибка при создании администратора в БД {string.Join(", ", creation_result.Errors.Select(e => e.Description))}");
             }
         }
     }
