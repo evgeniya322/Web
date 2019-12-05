ef550df9bc7b77ceb431e31ca36e36b8589878bf
Евгения <zbizk322@mail.ru>
2019-12-05 13:25:40 +0700

    Список товаров для администратора

--- WStore/WStore/Areas/Admin/Controllers/HomeController.cs
+++ WStore/WStore/Areas/Admin/Controllers/HomeController.cs
@@ -2,16 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.Entities.Identity;
+using WStore.Infrasructure.Interface;
 
 namespace WStore.Areas.Admin.Controllers
 {
-    [Area("Admin")]
+    [Area("Admin"), Authorize(Roles = Role.Administrator)]
     public class HomeController : Controller
     {
+        private readonly IProductData _ProductData;
+        public HomeController(IProductData ProductData) => _ProductData = ProductData;
         public IActionResult Index()
         {
             return View();
         }
+
+        public IActionResult ProductList() => View(_ProductData.GetProducts());
+
+        public IActionResult Edit(int? id) => View();
+
+        public IActionResult Delete(int id) => View();
+
+        [HttpPost, ActionName(nameof(Delete))]
+        public IActionResult DeleteConfirm(int id) => RedirectToAction("ProductList");
     }
 }
